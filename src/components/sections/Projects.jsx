2365e4c3e060333b7177700505f5c7022eda3267
foleyd2002 <foleydarragh37@gmail.com>
2025-03-02 18:33:29 +0000

    about page

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,9 @@
+export const Projects = () => {
+
+    return (
+        <section 
+            id="projects" className="min-h-screen flex items-center justify-center py-20">
+
+        </section>
+    );  
+};
